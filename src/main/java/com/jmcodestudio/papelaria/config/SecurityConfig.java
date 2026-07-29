c18928bc190164beee259f39aa602a9c5e88f11c
jbbastos1303 <jbbastos.dev@gmail.com>
2026-07-29 16:42:54 -0300

    implementacao de ajuda para o usuario correcoes

--- src/main/java/com/jmcodestudio/papelaria/config/SecurityConfig.java
+++ src/main/java/com/jmcodestudio/papelaria/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
             .headers(headers -> headers
                 .contentSecurityPolicy(csp -> csp.policyDirectives(
                     "default-src 'self'; " +
-                    "script-src 'self' 'unsafe-inline'; " +
+                    "script-src 'self' 'unsafe-inline' https://unpkg.com; " +
                     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
                     "font-src 'self' https://fonts.gstatic.com; " +
                     "img-src 'self' data: https:; " +
